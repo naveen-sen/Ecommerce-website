50c4fc57b4b49e19a217a13e7d29af72e0832db1
naveen-sen <naveennapit@gmail.com>
2025-05-07 23:35:44 +0530

    Fixed Server

--- Frontend/src/config/axiosConfig.js
+++ Frontend/src/config/axiosConfig.js
@@ -1,11 +1,11 @@
 import axios from 'axios'
 
-export const BASE_URL = "http://localhost:5000"
+export const BASE_URL = import.meta.env.MODE==="development" ? "http://localhost:5000" : "/"
 
 const token = localStorage.getItem("jwt")
 
 export const api = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? BASE_URL : "/",
+    baseURL: BASE_URL,
     headers:{
         "authorization":`Bearer ${token}`,
         "Content-Type":"application/json"
