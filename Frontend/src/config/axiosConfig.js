8bf308d206fdb53a20b747fbea533fa2e15898c4
naveen-sen <naveennapit@gmail.com>
2025-05-08 21:28:51 +0530

    Project completed

--- Frontend/src/config/axiosConfig.js
+++ Frontend/src/config/axiosConfig.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const token = localStorage.getItem("jwt")
 
 export const api = axios.create({
-    baseURL: import.meta.env.MODE==="development" ? "http://localhost:3000" : "/",
+    baseURL: import.meta.env.MODE==="development" ? "http://localhost:3000" : "https://thetrendycart.onrender.com",
     headers:{
         "authorization":`Bearer ${token}`,
         "Content-Type":"application/json"
